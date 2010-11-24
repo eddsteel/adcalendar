f2d73162af46623e766ca68a57abf5108371bb97
Edd Steel <edward.steel@gmail.com>
2010-11-24 18:24:05 +0000

    add squaring of boxes

--- public/behaviour.js
+++ public/behaviour.js
@@ -2,38 +2,47 @@ var clickedBox = null;
 
 (function(){
 
-	/*
-	 * Open a JQuery'ed box
-	 */
-	var click_open = function(ev) {
+  /*
+   * Open a JQuery'ed box
+   */
+  var click_open = function(ev) {
     clickedBox = ev.currentTarget;
-		open(clickedBox);
-	}
+    open(clickedBox);
+  }
 
   function open(box) {
-		var current = box.className;
+    var current = box.className;
     $(box).attr('className', current.replace('closed', 'open'));
   }
 
   function preopen_closed() {
-  	var today = 12; // TODO
-		var text;
-		var boxes = $('.closed');
-		boxes.each(function(b){
-			text = $(boxes[b]).text().trim();
-			if (today > text)
-			{
-				open(boxes[b]);
-			}
-		});
-	}
+    var today = 12; // TODO
+    var text;
+    var boxes = $('.closed');
+    boxes.each(function(b){
+      text = $(boxes[b]).text().trim();
+      if (today > text)
+      {
+        open(boxes[b]);
+      }
+    });
+  }
 
+  function square_boxes(){
+    // get width of boxes
+    var dim = $('.box').css('width');
+
+    // apply height, line height and font-size to all boxes
+    $('.box').css('height', dim);
+    $('.box').css('line-height', dim);
+    $('.box').css('font-size', dim / 3);
+  }
 
   var start = function() {
-		preopen_closed();
-		resize_boxes();
-		$('.closed').click(click_open);
-	}
+    preopen_closed();
+    square_boxes();
+    $('.closed').click(click_open);
+  }
 
   $(document).ready(start);
 
